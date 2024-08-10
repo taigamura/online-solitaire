2f776cdc0f789ebdc112532a4ab501cc36466d11
unknown <taigakimura97@gmail.com>
2024-08-11 02:12:32 +0900

    initial commit with simple app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import Draggable from 'react-draggable'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class based component
+class App extends Component {
+  
+  state = {
+    files: []
+  }
+
+  deckShow = false
+
+  handleChange = (e) => {
+    this.setState({ files: [...this.state.files, ...e.target.files] }, () => {
+      console.log(this.state)
+    })
+  }
+
+  handleSubmit = (e) => {
+    // reloads page for some reason default
+    e.preventDefault();
+    this.deckShow = true
+    console.log(this.deckShow)
+    console.log(e)
+  }
+
+  handleReset = (e) => {
+    this.deckShow = false
+    this.state.files = []
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>カードを追加</h3>
+        <input type="file" multiple onChange={this.handleChange} />
+        
+        <Draggable>
+          <div className="App">
+            {/* デュエマは 63mm x 88mm */}
+            {this.state.files.map( (file, index) => (
+              <div>
+                <img src={URL.createObjectURL(file)} width="157.5" height="220" alt="error" />
+                <input type="number" id={index} name="number"/>
+              </div>
+            ))}
+          </div>
+        </Draggable>
+        
+        <form onSubmit={this.handleSubmit}>
+          <button type = 'submit'>カード枚数確定</button>
+        </form>
+
+        <form onSubmit={this.handleReset}>
+          <button type = 'submit'>リセット</button>
+        </form>
+
+      </div>
+    );
+  }
 }
 
 export default App;
