d4813eefbdab89c5f4015f87b44beea2c83f591d
unknown <taigakimura97@gmail.com>
2024-08-11 14:22:59 +0900

    function component, failing

--- src/App.js
+++ src/App.js
@@ -1,110 +1,97 @@
 import './App.css';
 import Draggable from 'react-draggable'
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import Play from './Play';
 
-// class based component
-class App extends Component {
-	constructor() {
-		super()
+// function component
+function App() {
 
-		this.state = {
-			files: [],
-			deckShow: false
+	const [files, setFiles] = useState();
+	const [deckShow, setDeckShow] = useState();
+
+	function handleChange(e){
+		// append if exists
+		if (files) {
+			setFiles([...files, ...e.target.files], () => {
+				files.array.forEach(element => {
+					element.number = 1
+				});
+			})
+		}
+		else {
+			setFiles([...e.target.files], () => {
+				files.array.forEach(element => {
+					element.number = 1
+				});
+			})
 		}
 	}
 
-	handleChange = (e) => {
-		this.setState({ files: [...this.state.files, ...e.target.files] }, () => {
-			this.state.files.forEach((e) => {
-				e.number = 1
-			})
-			this.forceUpdate()
-		})
-	}
-
-	handleSubmit = (e) => {
+	function handleSubmit(e){
 		// reloads page for some reason default
 		e.preventDefault();
-		this.setState({ deckShow: true })
+		setDeckShow(true)
 	}
 
-	handleReset = (e) => {
-		this.setState({ deckShow: false })
-		this.setState({ files: [] })
+	function handleReset(e){
+		setDeckShow(false)
+		setFiles([])
 	}
 
-	handleNumberChange = (e) => {
-
-		this.setState(({ files }) => ({
-			...files[e.target.id].number = parseInt(e.target.value)
-		}))
-
-		console.log(this.state)
+	function handleNumberChange(e){
+		console.log(e.target.id)
+		setFiles(files[e.target.id].number = parseInt(e.target.value))
+	}
+	
+	function handleRedirect(e){
+		return
 	}
 
-	handleRedirect = (e) => {
-		const navigate = useNavigate()
-		e.preventDefault();
-		navigate("/play")
-	}
+	return (
+		<div>
+			<h3>カードを追加</h3>
+			<input type="file" multiple onChange={handleChange} />
 
-	render() {
-		return (
-			<div>
-				<h3>カードを追加</h3>
-				<input type="file" multiple onChange={this.handleChange} />
-
-				<Draggable>
-					<div className="App">
-						{/* デュエマは 63mm x 88mm */}
-						{this.state.files.map((file, index) => (
-							<div>
-								<img src={URL.createObjectURL(file)} width="157.5" height="220" alt="error" />
-								<select id={index} placeholder={file.number} onChange={this.handleNumberChange}>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-									<option value="4">4</option>
-								</select>
-							</div>
-						))}
-					</div>
-				</Draggable>
-
-				<form onSubmit={this.handleSubmit}>
-					<button type='submit'>カード枚数確定</button>
-				</form>
-
-				<form onSubmit={this.handleReset}>
-					<button type='submit'>リセット</button>
-				</form>
-
-				{this.state.deckShow && <div>
-					{this.state.files.map((file, index) => (
-						[...Array(file.number)].map(() => (
-							<div>
-								<img src={URL.createObjectURL(file)} width="157.5" height="220" alt="error" />
-							</div>
-						))
+			<Draggable>
+				<div className="App">
+					{/* デュエマは 63mm x 88mm */}
+					{files?.map((file, index) => (
+						<div>
+							<img src={URL.createObjectURL(file)} width="157.5" height="220" alt="error" />
+							<select id={index} placeholder={file.number} onChange={handleNumberChange}>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+							</select>
+						</div>
 					))}
+				</div>
+			</Draggable>
 
-					<form onSubmit={this.handleRedirect}>
-						<button type='submit'>デッキ確定</button>
-					</form>
-				</div>}
+			<form onSubmit={handleSubmit}>
+				<button type='submit'>カード枚数確定</button>
+			</form>
 
-				<BrowserRouter>
-					<Routes>
-						<Route exact={true} path="/play" component={Play} />
-					</Routes>
-				</BrowserRouter>
+			<form onSubmit={handleReset}>
+				<button type='submit'>リセット</button>
+			</form>
 
-			</div>
-		);
-	}
+			{deckShow && <div>
+				{files?.map((file, index) => (
+					[...Array(file.number)].map(() => (
+						<div>
+							<img src={URL.createObjectURL(file)} width="157.5" height="220" alt="error" />
+						</div>
+					))
+				))}
+
+				<form onSubmit={handleRedirect}>
+					<button type='submit'>デッキ確定</button>
+				</form>
+			</div>}
+		</div>
+	)
 }
 
 export default App;
